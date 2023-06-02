7595d71a77d565b63a9170ae4d5ed99e7466ba64
fonsecaBarreto <lucasbfonte@gmail.com>
2023-06-01 22:10:16 -0300

    feat: add json parser

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
 const express = require('express')
+const { csvStringToJson } = require("./parsers");
 const app = express()
 const port = 3000
 
 app.use(express.text())
 
 app.post('/csv', (req, res) => {
-  console.log("data received", req.body);
+  console.log(new Date().toLocaleString(), " - data received:\n");
+  const json = csvStringToJson(req.body);
+  console.log(json);
   return res.send("ok");
 })
 
---
+++ server/parsers.js
@@ -0,0 +1,22 @@
+
+function csvStringToJson(csvString) {
+  const lines = csvString.split('\n');
+  const headers = lines[0].split(',');
+
+  const result = [];
+
+  for (let i = 1; i < lines.length; i++) {
+    const currentLine = lines[i].split(',');
+    if (currentLine.length === headers.length) {
+      const obj = {};
+      for (let j = 0; j < headers.length; j++) {
+        obj[headers[j]] = currentLine[j];
+      }
+      result.push(obj);
+    }
+  }
+
+  return JSON.stringify(result, null, 4);
+}
+
+module.exports = { csvStringToJson };
