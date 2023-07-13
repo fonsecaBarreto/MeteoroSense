c2fd32340a947ec2079169b823389fbc4a391e0d
Gabriel Fonseca <gabrielfonsecabarreto@gmail.com>
2023-07-13 12:30:59 -0300

    feat: add NTPClient integraton

--- arduino/index/integration.h
+++ arduino/index/integration.h
@@ -3,6 +3,9 @@
 // Data: 01/06/2023
 //.........................................................................................................................
 #pragma once
+//#include <ESP8266WiFi.h>
+//#include <WiFiUdp.h>
+#include <NTPClient.h>
 #include <WiFi.h>
 #include <HTTPClient.h>
 #include <ArduinoMqttClient.h>
@@ -16,6 +19,10 @@ const String API_URL = "http://192.168.0.173:3000/csv";
 const char broker[] = "telemetria.macae.ufrj.br";
 int port = 1883;
 const char topic[] = "/prefeituras/macae/estacoes/est001";
+
+
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP);
 
 WiFiClient wifiClient;
 MqttClient mqttClient(wifiClient);
@@ -33,9 +40,15 @@ int connectWifi() {
   Serial.println("\nConnected to the WiFi network");
   Serial.print("Local ESP32 IP: ");
   Serial.println(WiFi.localIP());
+
   return 1;
 }
 
+int connectNtp(){
+  Serial.println("\nStarting ntp connection");
+  timeClient.begin();
+  return 1;
+}
 int connectMqtt()
 {
   Serial.print("Attempting to connect to the MQTT broker: ");
