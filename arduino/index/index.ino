d80f04c6363bbbb607e7d382a03eac47bd079dd9
Gabriel Fonseca <gabrielfonsecabarreto@gmail.com>
2023-07-13 12:32:36 -0300

    refactor: fix json output sintax

--- arduino/index/index.ino
+++ arduino/index/index.ino
@@ -12,11 +12,13 @@
 #define VANE_PIN 36
 #define DHTTYPE DHT11  // Define o tipo de sensor (DHT11 ou DHT22)
 // constants
-#define INTERVAL 5000  // Intervalo de Tempo entre medições (ms)
+#define STATION_ID "001"
+#define STATION_P "est001"
+#define INTERVAL 60000  // Intervalo de Tempo entre medições (ms)
 #define DEBOUNCE_DELAY 25
 
 // Anemometro (Velocidade do vento)
-#define ANEMOMETER_CIRC (2 * 3.14159265 * 0.145)  // Circunferência anemometro (m)
+#define ANEMOMETER_CIRC (2.0 * 3.14159265 * 0.085)  // Circunferência anemometro (m)
 unsigned long lastVVTImpulseTime = 0;
 unsigned int anemometerCounter = 0;
 
@@ -26,12 +28,13 @@ unsigned long lastPVLImpulseTime = 0;
 unsigned rainCounter = 0;
 
 // Biruta (Direção do vento)
-#define NUMDIRS 16
-int adc[NUMDIRS] = {144,   50,   475,  260,  860,  700,  2190,  2070, 3750, 3020, 3350, 2489, 2832,  1315, 1530, 100};
-char* strVals[NUMDIRS] = {"N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW"};
+#define NUMDIRS 8
+//int adc2[NUMDIRS] = {144, 50, 475, 260, 860, 700, 2190, 2070, 3750, 3020, 3350, 2489, 2832, 1315, 1530, 100 };
+//char* strVals2[NUMDIRS] = {"N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW" };
+int adc[NUMDIRS] = {480,600,800,1150,1820,250,305,380};
+char* strVals[NUMDIRS] = {"N", "NE", "E", "SE", "S", "SW", "W", "NW" };
 unsigned int vane_dir = 0;
 byte dirOffset = 0;
-
 // Temperatura, umidade
 DHT dht(DHTPIN, DHTTYPE);
 
@@ -49,27 +52,26 @@ struct
 char json_output[160];
 char csv_output[160];
 
-void DataToJson(long startTime){
-  const char* csv_header ="{ \"timestamp\": %d, \"wind_speed\": %.2f, \"rain_cc\": %.2f, \"humidity\": %.2f, \"temperature\": %.2f }";
-  sprintf(json_output, csv_header, startTime + INTERVAL, Data.wind_speed, Data.rain_acc, Data.humidity,Data.temperature);
+void DataToJson(long timestamp){
+  const char* csv_header = "{\"timestamp\": %i, \"temperatura\": %.2f, \"umidade_ar\": %.2f, \"velocidade_vento\": %.2f, \"dir_vento\": %d, \"volume_chuva\": %.2f, \"uid\": \"%s\", \"identidade\": \"%s\"}";
+  sprintf(json_output, csv_header,timestamp,Data.temperature,Data.humidity, Data.wind_speed,Data.wind_dir, Data.rain_acc,STATION_ID,STATION_P );
 }
 
 void DataToCsv(long startTime){
   const char* csv_header ="timestamp,wind_speed,rain_cc,humidity,temperature\n%d,%.2f,%.2f,%.2f,%.2f";
   sprintf(csv_output, csv_header, startTime + INTERVAL, Data.wind_speed, Data.rain_acc, Data.humidity,Data.temperature);
 }
-//
 
 void setup() {
   Serial.begin(115200);
   delay(1000);
   Serial.println("\n\n Sistema Integrado de meteorologia \n");
   connectWifi();
+  connectNtp();
   connectMqtt();
   // pinos
   pinMode(ANEMOMETER_PIN, INPUT_PULLUP);
   pinMode(PLV_PIN, INPUT_PULLUP);
-
   // initial setup
   int now = millis();
   lastVVTImpulseTime = now;
@@ -79,9 +81,12 @@ void setup() {
   dht.begin();
 }
 
-
 void loop() {
-  Serial.println("\n");
+
+  timeClient.update();
+  int timestamp = timeClient.getEpochTime();
+  Serial.println("Iniciando medição em:");
+  Serial.print(timestamp);
 
   anemometerCounter = 0;
   rainCounter = 0;
@@ -107,13 +112,16 @@ void loop() {
   Serial.print(Data.temperature);
   Serial.println("°C");
   Serial.print("Direção do vento...:  ");
+  Serial.print(strVals[Data.wind_dir]);
   Serial.println(Data.wind_dir);
 
-  DataToJson(startTime);
+  DataToJson(timestamp);
   Serial.println(json_output);
+
   Serial.print("Enviando...........:  ");
   sendMeasurementToMqtt(json_output);
-  Serial.println("ok");
+
+  Serial.println("ok\n");
 }
 
 int getWindDir() {
@@ -123,11 +131,11 @@ int getWindDir() {
   int closestDifference = std::abs(val - adc[0]);
 
   for (int i = 1; i < 16; i++) {
-        int difference = std::abs(val - adc[i]);
-        if (difference < closestDifference) {
-            closestDifference = difference;
-            closestIndex = i;
-        }
+    int difference = std::abs(val - adc[i]);
+    if (difference < closestDifference) {
+      closestDifference = difference;
+      closestIndex = i;
+    }
   }
   return closestIndex;
 }
@@ -149,7 +157,6 @@ void pluviometerChange() {
 }
 
 void DHTRead() {
-
   float humidity = dht.readHumidity();        // umidade relativa
   float temperature = dht.readTemperature();  //  temperatura em graus Celsius
 
