bd8a7a1d9d57723aaefe9ff0251183a4919dfdaa
fonsecaBarreto <lucasbfonte@gmail.com>
2023-08-02 10:00:13 -0300

    chore: qldb config

--- arduino/index/conf.h
+++ arduino/index/conf.h
@@ -3,10 +3,10 @@ const char *ssid = "Gabriel";
 const char *password = "2014072276";
 
 /******* MQTT Broker Connection Details *******/
-const char *mqtt_server = "192.168.0.173"; // "telemetria.macae.ufrj.br"
+const char *mqtt_server = "telemetria.macae.ufrj.br";
 const char *mqtt_username = "lucas";
 const char *mqtt_password = "1234567";
-const char *mqtt_topic = "test"; //  "/prefeituras/macae/estacoes/est001";
+const char *mqtt_topic = "/prefeituras/macae/estacoes/est001";
 const int mqtt_port = 8883;
 
 /****** root certificate *********/
