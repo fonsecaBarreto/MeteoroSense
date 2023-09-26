b8669a6ccfc8118b59f2f3765be0c4950e7366f1
fonsecaBarreto <lucasbfonte@gmail.com>
2023-09-26 12:19:11 -0300

    !refactor: Move development services to debug directory.

--- debug/server/src/presentation/http/http-server.js
+++ debug/server/src/presentation/http/http-server.js
@@ -6,12 +6,16 @@ export function startHttpServer(port) {
 
   app.use(express.text());
 
-  app.post("/csv", (req, res) => {
-    console.log(new Date().toLocaleString(), " - data received:\n");
-    const json = csvStringToJson(req.body);
-    console.log(json);
+  app.get("/", (req, res) => {
     return res.send("ok");
   });
+
+ app.post("/csv", (req, res) => {
+   console.log(new Date().toLocaleString(), " - data received:\n");
+   const json = csvStringToJson(req.body);
+   console.log(json);
+   return res.send("ok");
+ }); 
 
   app.listen(port, () => {
     console.log(`Listening on port ${port}`);
