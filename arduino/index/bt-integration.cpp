7aac47d2eca689b2da4734698e90c8d1b932ab8b
fonsecaBarreto <lucasbfonte@gmail.com>
2023-10-16 08:20:56 -0300

    feat: add new bt characteristic

--- arduino/index/bt-integration.cpp
+++ arduino/index/bt-integration.cpp
@@ -5,83 +5,95 @@
 #include <BLEUtils.h>
 #include <BLE2902.h>
 
-BLEServer* pServer = nullptr;
-BLECharacteristic* pConfigCharacteristic = nullptr;
+// Station BLE service & characteristics
+#define SERVICE_UUID "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+#define CONFIGURATION_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+#define HEALTH_CHECK_UUID "7c4c8722-8b05-4cca-b5d2-05ec864f90ee"
+
+BLEServer *pServer = nullptr;
+BLECharacteristic *pConfigCharacteristic = nullptr;
+BLECharacteristic *pHealthCharacteristic = nullptr;
 bool deviceConnected = false;
-bool oldDeviceConnected = false;
-std::string currentConfig;
+int (*jscb)(const std::string &json);
 
-// callbacks
-int(*jscb)(const std::string& json);
-
-#define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
-#define CONFIGURATION_UUID  "beb5483e-36e1-4688-b7f5-ea07361b26a8"
-
-class MyServerCallbacks: public BLEServerCallbacks {
-    void onConnect(BLEServer* pServer) {
+class MyServerCallbacks : public BLEServerCallbacks
+{
+    void onConnect(BLEServer *pServer){
         std::cout << "Novo aparelho conectado.\n";
         deviceConnected = true;
     };
 
-    void onDisconnect(BLEServer* pServer) {
+    void onDisconnect(BLEServer *pServer){
         deviceConnected = false;
         BLEDevice::startAdvertising();
     }
-
 };
 
 class MyCallbacks : public BLECharacteristicCallbacks {
     void onWrite(BLECharacteristic *pCharacteristic) {
         std::string rxValue = pCharacteristic->getValue();
-        if (!rxValue.empty()) {
-            // currentConfig = rxValue;
+        if (!rxValue.empty()){
             std::cout << "Received message: " << std::endl;
             std::cout << rxValue << std::endl;
-            if(jscb)jscb(rxValue);
-            // Process the received message as needed
+            if (jscb) {
+                jscb(rxValue);
+            }
         }
     }
 };
 
-void BLE::SetConfigCallback(int(*jsCallback)(const std::string& json)){jscb=jsCallback;}
+void BLE::SetConfigCallback(int (*jsCallback)(const std::string &json)) { jscb = jsCallback; }
 
-void BLE::Init(const char* boardName, const std::string& currentConfig) {
-  std::cout<<currentConfig<<std::endl;
-  // Create the BLE Device
-  BLEDevice::init(boardName);
+void BLE::Init(const char *boardName, const std::string &currentConfig) {
+    std::cout << currentConfig << std::endl;
+    // Create the BLE Device
+    BLEDevice::init(boardName);
 
-  // Create the BLE Server
-  pServer = BLEDevice::createServer();
-  pServer->setCallbacks(new MyServerCallbacks());
+    // Create the BLE Server
+    pServer = BLEDevice::createServer();
+    pServer->setCallbacks(new MyServerCallbacks());
 
-  // Create the BLE Service
-  BLEService *pService = pServer->createService(SERVICE_UUID);
+    // Create the BLE Service
+    BLEService *pService = pServer->createService(SERVICE_UUID);
 
-  // Create the Configuration Characteristic
-  pConfigCharacteristic = pService->createCharacteristic(
-      CONFIGURATION_UUID,
-      BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_NOTIFY
-  );
+    // Create the Configuration Characteristic
+    pConfigCharacteristic = pService->createCharacteristic(
+        CONFIGURATION_UUID,
+        BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_NOTIFY);
 
-  BLEDescriptor *pDescriptor = new BLEDescriptor(BLEUUID((uint16_t)0x2901));
-  pDescriptor->setValue("Estacao");
+    pHealthCharacteristic = pService->createCharacteristic(
+        HEALTH_CHECK_UUID,
+        BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_NOTIFY);
 
-  pConfigCharacteristic->addDescriptor(pDescriptor);
-  pConfigCharacteristic->setCallbacks(new MyCallbacks());
-  if (currentConfig.length()>0) {
-    std::cout<<"DoingSomething\n";
-    pConfigCharacteristic->setValue(currentConfig);
-  }
-  // Start the service
-  pService->start();
+    pConfigCharacteristic->setCallbacks(new MyCallbacks());
+    if (currentConfig.length() > 0) {
+        std::cout << "DoingSomething\n";
+        pConfigCharacteristic->setValue(currentConfig);
+    }
 
-  // Start advertising
-  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
-  pAdvertising->addServiceUUID(SERVICE_UUID);
-  pAdvertising->setScanResponse(false);
-  pAdvertising->setMinPreferred(0x0);  // set value to 0x00 to not advertise this parameter
+    pHealthCharacteristic->setCallbacks(new MyCallbacks());
+    pHealthCharacteristic->setValue("VALOR PARA HEALTH CHECK");
 
-  BLEDevice::startAdvertising();
-  std::cout <<"Waiting a client set json\n";
+    // Start the service
+    pService->start();
 
+    // Start advertising
+    BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+    pAdvertising->addServiceUUID(SERVICE_UUID);
+    pAdvertising->setScanResponse(false);
+    pAdvertising->setMinPreferred(0x0); // set value to 0x00 to not advertise this parameter
+
+    BLEDevice::startAdvertising();
+    std::cout << "Waiting a client set json\n";
+}
+
+void BLE::updateValue(const char *characteristicId, const std::string &newValue){
+    if (newValue.length() > 0) {
+        std::cout << "New value to characteristic\n";
+        pHealthCharacteristic->setValue(newValue);
+    }
+}
+
+bool BLE::isDeviceConnected(){
+    return deviceConnected;
 }
--- arduino/index/bt-integration.h
+++ arduino/index/bt-integration.h
@@ -5,6 +5,8 @@ class BLE {
   public:
   static void Init(const char* boardName,  const std::string& currentConfig);
   static void SetConfigCallback(int(*jsCallback)(const std::string& json));
+  static void updateValue(const char *characteristicId, const std::string &newValue);
+  static bool isDeviceConnected();
 };
 
 
