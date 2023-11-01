6d3d19248a299652787f8b5cbe22001d25ca5d14
fonsecaBarreto <lucasbfonte@gmail.com>
2023-11-01 16:04:23 -0300

    refactor: index.ino

--- arduino/index/index.ino
+++ arduino/index/index.ino
@@ -11,10 +11,10 @@
 #include <stdio.h>
 #include "esp_system.h"
 #include "bt-integration.h"
-long startTime;
-
 #include <string>
 #include <vector>
+
+long startTime;
 
 // Pluviometro
 extern unsigned long lastPVLImpulseTime;
@@ -34,6 +34,8 @@ bool forceRestart = false;
 String formatedDateString = "";
 int timeRemaining=0;
 std::string jsonConfig;
+
+struct HealthCheck healthCheck = {"1.6", 0, false, false, 0, 0};
 
 void logIt(const std::string &message, bool store = false){
   Serial.print(message.c_str());
@@ -64,8 +66,8 @@ void setup() {
   loadConfiguration(SD, configFileName, config, jsonConfig);
 
   logIt("\n1.1 Iniciando bluetooth;", true);
-  BLE::SetConfigCallback(SaveConfigFile);
-  BLE::Init(config.station_name, jsonConfig);
+  BLE::Init(config.station_name, bluetoothController);
+  BLE::updateValue(CONFIGURATION_UUID, jsonConfig);
 
   logIt("\n1.2 Estabelecendo conexão com wifi ", true);
   setupWifi("  - Wifi", config.wifi_ssid, config.wifi_password);
@@ -84,70 +86,71 @@ void setup() {
   int now = millis();
   lastVVTImpulseTime = now;
   lastPVLImpulseTime = now;
-  startTime = now;
 
   // 2; Inicio
   Serial.printf("\n >> PRIMEIRA ITERAÇÃO\n");
+
   int timestamp = timeClient.getEpochTime();
   convertTimeToLocaleDate(timestamp);
+  
   String dataHora = String(formatedDateString) + "T" + timeClient.getFormattedTime();
   storeLog(("\n" + dataHora + "\n").c_str());
-  healthCheck.timestamp = timestamp;
 }
+
+
 
 void loop() {
 
-  // Health check
-  healthCheck.isWifiConnected = WiFi.status() == WL_CONNECTED;
-  healthCheck.wifiDbmLevel = !healthCheck.isWifiConnected ? 0 : (WiFi.RSSI()) * -1;
-  healthCheck.isMqttConnected = mqttClient.loop();
-  healthCheck.timeRemaining = timeRemaining;
+  startTime = millis();
 
-  const char * hcJson = healthCheck.toJson();
-
-  Serial.printf("\n\nColetando dados, proximo resultado em %d segundos...", (timeRemaining / 1000));
-  Serial.printf("\n%s",hcJson);
-
-  // Garantindo conexão com mqqt broker;
-  if (healthCheck.isWifiConnected && !healthCheck.isMqttConnected) {
-    healthCheck.isMqttConnected = connectMqtt("\n  - MQTT", config.mqtt_username, config.mqtt_password, config.mqtt_topic);
-  }
-
-  // Atualizando BLE advertsting value
-  if (BLE::isDeviceConnected()){
-    BLE::updateValue(HEALTH_CHECK_UUID, hcJson);
-  }
-
-  // Garantindo Tempo ocioso para captação de metricas 60s
-  timeRemaining = startTime + config.interval - millis();
-  if (timeRemaining > 0) {
-    unsigned long startMillis = millis();
-    while (millis() - startMillis < 5000);
-    return;
-  }
-
-  // Ping NTP
   timeClient.update();
   int timestamp = timeClient.getEpochTime();
+
   convertTimeToLocaleDate(timestamp);
-  healthCheck.timestamp = timestamp;
+
+  rainCounter = 0;
+  anemometerCounter = 0;
+  smallestDeltatime = 4294967295;
+
+  do {
+    timeRemaining = startTime + config.interval - millis();
+    if(ceil(timeRemaining % 5000) != 0) continue;
+
+    // Health check
+    healthCheck.timestamp = timestamp;
+    healthCheck.isWifiConnected = WiFi.status() == WL_CONNECTED;
+    healthCheck.wifiDbmLevel = !healthCheck.isWifiConnected ? 0 : (WiFi.RSSI()) * -1;
+    healthCheck.isMqttConnected = mqttClient.loop();
+    healthCheck.timeRemaining = timeRemaining;
+
+    const char * hcCsv = parseHealthCheckData(healthCheck, 1);
+    const char * hcJson = parseHealthCheckData(healthCheck, 2);
+
+    Serial.printf("\n\nColetando dados, metricas em %d segundos ...", (timeRemaining / 1000));
+    Serial.printf("\n%s",hcJson);
+
+    // Garantindo conexão com mqqt broker;
+    if (healthCheck.isWifiConnected && !healthCheck.isMqttConnected) {
+      healthCheck.isMqttConnected = connectMqtt("\n  - MQTT", config.mqtt_username, config.mqtt_password, config.mqtt_topic);
+    }
+
+    // Atualizando BLE advertsting value
+    BLE::updateValue(HEALTH_CHECK_UUID, ("HC: " + String(hcCsv)).c_str());
+
+    // Garantindo Tempo ocioso para captação de metricas 60s
+  } while (timeRemaining > 0);
 
   // Computando dados
   Serial.printf("\n\n Computando dados ...\n");
 
   Data.timestamp = timestamp;
   Data.wind_dir = getWindDir();
-  Data.wind_speed = 3.052 * (ANEMOMETER_CIRC * anemometerCounter) / (INTERVAL / 1000.0); // m/s
-  Data.wind_gust = (3052.0f * ANEMOMETER_CIRC) / smallestDeltatime;
   Data.rain_acc = rainCounter * VOLUME_PLUVIOMETRO;
+  Data.wind_gust = (3052.0f * ANEMOMETER_CIRC) / smallestDeltatime;
+  Data.wind_speed = 3.052 * (ANEMOMETER_CIRC * anemometerCounter) / (INTERVAL / 1000.0); // m/s
+  
   DHTRead(Data.humidity, Data.temperature);
   BMPRead(Data.pressure);
-
-  // Redefinido variaveis de medição
-  startTime = millis();
-  anemometerCounter = 0;
-  rainCounter = 0;
-  smallestDeltatime = 4294967295;
 
   // 8. Apresentação dos Dados
   parseData();
@@ -163,22 +166,23 @@ void loop() {
   bool measurementSent = sendMeasurementToMqtt(config.mqtt_topic, metricsjsonOutput);
 
   // Update metrics advertsting value
-  if (BLE::isDeviceConnected()){
-    BLE::updateValue(METRICS_UUID, metricsjsonOutput);
-  }
-
+  BLE::updateValue(HEALTH_CHECK_UUID, ("ME: " + String(metricsCsvOutput)).c_str());
   Serial.printf("\n >> PROXIMA ITERAÇÃO\n");
+
+  delay(5000);
 }
 
 // callbacks
-int SaveConfigFile(const std::string &json) {
-  createFile(SD, "/config.txt", json.c_str());
-
-  logIt("Configuração alterar via bluetooth", true);
-  logIt("Reiniciando Arduino a força;", true);
-  delay(1000);
-  ESP.restart();
-  return 1;
+int bluetoothController(const char *uid, const std::string &content) {
+  if (uid == CONFIGURATION_UUID) {
+    createFile(SD, "/config.txt", content.c_str());
+    logIt("Configuração alterar via bluetooth", true);
+    logIt("Reiniciando Arduino a força;", true);
+    delay(1000);
+    ESP.restart();
+    return 1;
+  }
+  return 0;
 }
 
 void convertTimeToLocaleDate(long timestamp) {
